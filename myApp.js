75590782caa4f8bd8545fc5a25de810a8ce8e774
WaNeko <78503871+WaNeko@users.noreply.github.com>
2021-07-28 11:13:42 -0300

    0.1.0

--- myApp.js
+++ myApp.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var app = express();
 
+app.route('/name').get((req,res)=>{
+    res.json({ name : `${req.query.first} ${req.query.last}`});
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.use('/public',express.static(__dirname + '/public'));
 
 
 
